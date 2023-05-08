e97475c5014ee8c7557ceb4fc9a2cf4452d83184
Sandaru Tharuneth <sandarutharuneth@gmail.com>
2023-05-08 16:53:10 +0530

    Update imagine.js
    String error fixed

--- src/slashCommands/information/imagine.js
+++ src/slashCommands/information/imagine.js
@@ -16,38 +16,36 @@ module.exports = {
     run: async (client, interaction, args) => {
         await interaction.deferReply()
         const prompt = interaction.options.getString("prompt")
-        const Replicate = (await import("replicate")).default
+        const Replicate = require('replicate')
+        //const Replicate = (await import("replicate")).default
 
         const replicate = new Replicate({
             auth: config.API,
         });
 
-        const output = await
-        replicate.run("prompthero/openjourney:9936c2001faa2194a261c01381f90e65261879985476014a0a37a334593a05eb", {
+        const output = await replicate.run("prompthero/openjourney:9936c2001faa2194a261c01381f90e65261879985476014a0a37a334593a05eb", {
             input: {
                 prompt: prompt
             }
         })
-	
-	if (interaction.channel.nsfw === true) {
+
+        console.log()
         
         const row = new ActionRowBuilder()
 			.addComponents(
                 new ButtonBuilder()
                 .setLabel(`Download`)
                 .setStyle(ButtonStyle.Link)
-                .setURL(output)
-                .setEmoji('1083007659457912852'),
+                .setURL(`${output}`),
                new ButtonBuilder()
                 .setLabel(`Support Us`)
                 .setStyle(ButtonStyle.Link)
-                .setURL('https://paypal.me/officialrazer')
-                .setEmoji('1083016028369453199'))
+                .setURL('https://paypal.me/officialrazer'))
             
         const embed = new EmbedBuilder()
         	.setTitle("**Your Prompt:**")
             .setDescription(`**${prompt}**`)
-            .setImage(output)
+            .setImage(`${output}`)
         	.setColor('#2f3136')
         	.setFooter({ text: `Requested by: ${interaction.user.username} | ©️ Project Razer `,
                             iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
@@ -55,14 +53,5 @@ module.exports = {
 
         await interaction.editReply({ embeds: [embed], components: [row] })
 	    
-	    }else {
-
-            const nsfw = new EmbedBuilder()
-	    	.setTitle('Warning')
-            	.setDescription('\`\`\`fix\n[ Must be a Age Restricted channel to function ]\n\`\`\`')
-        	.setColor('#2f3136')
-
-          await interaction.editReply({ embeds: [nsfw] })
-           }
     }
 }
